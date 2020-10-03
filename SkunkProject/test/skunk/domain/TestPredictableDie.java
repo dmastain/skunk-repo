4616390810f1524c46bf10f7ea646aee03e7e652
Daniel Mastain <dmastain@gmail.com>
2020-10-02 20:25:18 -0500

    First passing test of test_PD_1_2_3

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDie {
+
+	@Test
+	void test_PD_1_2_3() {
+		PredictableDie die = new PredictableDie(new int[] {1,2,3});
+		die.roll();
+		assertEquals(1, die.getLastRoll());
+	}
+
+}
