f91698e45e1b82d87c07c144cb7407472dc27d83
Daniel Mastain <dmastain@gmail.com>
2020-10-23 14:35:40 -0500

    Passing bounds check test

--- SkunkProject/test/skunk/domain/TestDie.java
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -7,9 +7,20 @@ public class TestDie
 {
 
 	@Test
-	public void test_roll_in_bounds()
+	public void test_roll_100_in_bounds()
 	{
-		fail("Not yet implemented");
+		int roll;
+		int i;
+		int pass = 0;
+		Die die = new Die();
+		for (i = 0; i < 100; i++) {
+			die.roll();
+			roll = die.getLastRoll();
+			if (roll >= 1 && roll <= 6) {
+				pass++;
+			}
+		}
+		assertEquals(i, pass);
 	}
 
 }
