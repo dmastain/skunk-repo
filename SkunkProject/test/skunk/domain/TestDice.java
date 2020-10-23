95a2a1eedd709c4ded74bf2a5fb691d5f1872a0a
Daniel Mastain <dmastain@gmail.com>
2020-10-23 15:21:59 -0500

    Dice tests fairness and in bounds

---
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -0,0 +1,39 @@
+package skunk.domain;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDice {
+	
+	@Test
+	public void test_roll_100_dice_in_bounds() {
+		int roll;
+		int i;
+		int pass = 0;
+		Dice dice = new Dice();
+		for (i = 0; i < 100; i++) {
+			dice.roll();
+			roll = dice.getLastRoll();
+			if (roll >= 2 && roll <= 12) {
+				pass++;
+			}
+		}
+		assertEquals(i, pass);
+	}
+	
+	@Test
+	public void test_roll_100_dice_fairness() {
+		int roll;
+		int i;
+		float average = 0;
+		int pass = 0;
+		Dice dice = new Dice();
+		for (i = 1; i <= 100; i++) {
+			dice.roll();
+			roll = dice.getLastRoll();
+			average += roll;
+		}
+		average = average / i;
+		assertEquals(7, Math.round(average));
+	}
+}
